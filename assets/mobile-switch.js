452a43212b8d2355a415c6d0b72cdb921b202679
Matthias Kaschubowski <me@golpha.com>
2013-08-12 05:19:36 +0200

    Update mobile-switch.js

--- assets/mobile-switch.js
+++ assets/mobile-switch.js
@@ -1,2 +1,2 @@
 $('#page-contents').insertBefore('#guide');
-$("meta[name='viewport']").attr('content','width=device-width, initial-scale=0.8, maximum-scale=1.0, minimum-scale=0.8');
+$("meta[name='viewport']").attr('content','width=device-width, initial-scale=1.3, maximum-scale=1.3, minimum-scale=1.0');

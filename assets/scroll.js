e360d2b00c7c1a1fea277268ef9c585363eb5100
hausl <j.haus@gmx.at>
2015-02-13 17:35:00 +0100

    Anker nach Scrollen in URL ergänzen

--- assets/scroll.js
+++ assets/scroll.js
@@ -7,7 +7,10 @@ $(function() {
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
             if ($target.length) {
                 var targetOffset = $target.offset().top;
-                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                // $('html,body').animate({scrollTop: targetOffset}, 1000);
+                $('html,body').animate(
+                    {scrollTop: targetOffset}, 1000, function() {location.hash = $target.selector;}
+                );
                 return false;
             }
         }

a1875f477b755e90e4cb4fbff9989ae34e035d2c
hausl <j.haus@gmx.at>
2015-02-13 16:57:31 +0100

    Test Scroller der nach scrollen den Anker in URL ergänzt

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,3 +1,4 @@
+/*
 $(function(){
     $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
@@ -11,4 +12,23 @@ $(function(){
             }
         }
     });
+});
+*/
+
+$(document).ready(function() {
+	// Alle internen Links auswählen
+	$('a[href*=#]').bind("click", function(event) {
+		// Standard Verhalten unterdrücken
+		event.preventDefault();
+		// Linkziel in Variable schreiben
+		var ziel = $(this).attr("href");
+		//Scrollen der Seite animieren, body benötigt für Safari
+		$('html,body').animate({
+			//Zum Ziel scrollen (Variable)
+			scrollTop: $(ziel).offset().top
+		// Dauer der Animation und Callbackfunktion die nach der Animation aufgerufen wird,
+		// sie stellt das Standardverhalten wieder her und ergänzt die URL
+		}, 2000 , function (){location.hash = ziel;});
+   });
+return false;
 });

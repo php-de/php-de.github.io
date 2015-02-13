b5f5555c6c858be0c81ed4f7ea14244be86e34b7
hausl <j.haus@gmx.at>
2015-02-13 17:07:22 +0100

    Revert "test"
    This reverts commit b8370965f97f91c9eade74819d247fc9cee5dacd.

--- assets/scroll.js
+++ assets/scroll.js
@@ -22,10 +22,9 @@ $(function() {
         && location.hostname == this.hostname) {
             var $target = $(this.hash);
             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-            var $atrgethash = $(this).attr('href');
             if ($target.length) {
                 var targetOffset = $target.offset().top;
-                $('html,body').animate({scrollTop: targetOffset}, 1000, function (){location.hash = $targethash;} );
+                $('html,body').animate({scrollTop: targetOffset}, 1000, function (){location.hash = $target;} );
                 return false;
             }
         }

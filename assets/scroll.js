5ba496c2ea782f6d06183ab4ae04fe957fca837a
hausl <j.haus@gmx.at>
2015-02-13 17:02:35 +0100

    test

--- assets/scroll.js
+++ assets/scroll.js
@@ -1,5 +1,5 @@
 /*
-$(function(){
+$(function() {
     $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
         && location.hostname == this.hostname) {
@@ -15,6 +15,24 @@ $(function(){
 });
 */
 
+
+$(function() {
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000, function (){location.hash = ziel;} );
+                return false;
+            }
+        }
+    });
+});
+
+
+/*
 $(document).ready(function() {
 	// Alle internen Links auswählen
 	$('a[href*=#]').bind("click", function(event) {
@@ -32,3 +50,4 @@ $(document).ready(function() {
    });
 return false;
 });
+*/
